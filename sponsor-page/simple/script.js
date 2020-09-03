ec9f8774fa852e9eb9c71a04bd39f8a0cf0a44ee
NuoZhen <70146546+NuoZhen@users.noreply.github.com>
2020-09-03 18:28:50 +0800

    Update script.js

--- sponsor-page/simple/script.js
+++ sponsor-page/simple/script.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
 	var QRBox	=	$('#QRBox');
 	var MainBox	=	$('#MainBox');
-	var BTCQR	=	'images/BTCQR.png';	// 二维码路径
+	var BTCQR	=	'';	// 二维码路径
 	var AliPayQR	=	'images/AliPayQR.png';
 	var WeChanQR	=	'images/WeChanSQ.png';
 
